c726506a6bccc3ebb2a06cff1359da47be051690
Rodney Lorrimar <dev@rodney.id.au>
2019-08-12 12:00:28 +1000

    Try to fix it

--- src/Snap/Wai.hs
+++ src/Snap/Wai.hs
@@ -14,8 +14,6 @@ import           Data.IORef               (IORef, newIORef, readIORef,
 import           Data.List                (foldl')
 import qualified Data.Map.Strict          as Map
 import           Data.Maybe               (fromMaybe)
-import qualified Data.Text                as T
-import qualified Data.Text.Encoding       as T
 import           Data.Word                (Word64)
 import qualified Network.HTTP.Types       as H
 import           Network.Wai              (Application, Request (..),
@@ -33,19 +31,22 @@ import qualified System.IO.Streams        as Stream (read, write)
 -- | Converts a 'Network.Wai.Application' to a 'Snap' handler.
 serveWai :: MonadSnap m => Application -> m ()
 serveWai app = do
+  liftIO $ putStrLn "serveWai start"
   req <- convertRequest <$> Snap.getRequest
+  liftIO $ putStrLn $ "converted request: " ++ show req
   rvar <- liftIO (newIORef id :: IO (IORef (Snap.Response -> Snap.Response)))
   ResponseReceived <- liftIO $ app req $ \res -> do
-      writeIORef rvar $ case res of
-        ResponseBuilder st hdrs builder ->
-          responseBuilder builder . responseBase st hdrs
-        ResponseStream  st hdrs strmbdy ->
-          responseStream st (isHead req) strmbdy . responseBase st hdrs
-        ResponseFile st hdrs path mpart ->
-          responseFile path mpart . responseBase st hdrs
-        ResponseRaw{} ->
-          error "ResponseRaw is not supported"
-      pure ResponseReceived
+    putStrLn "Response received from app"
+    writeIORef rvar $ case res of
+      ResponseBuilder st hdrs builder ->
+        responseBuilder builder . responseBase st hdrs
+      ResponseStream st hdrs strmbdy ->
+        responseStream st (isHead req) strmbdy . responseBase st hdrs
+      ResponseFile st hdrs path mpart ->
+        responseFile path mpart . responseBase st hdrs
+      ResponseRaw{} ->
+        error "ResponseRaw is not supported"
+    pure ResponseReceived
   liftIO (readIORef rvar) >>= modifyResponse
 
 ----------------------------------------------------------------------------
@@ -60,19 +61,24 @@ convertRequest req@Request{..} = defaultRequest
   , requestHeaders = convertRequestHeaders $ Snap.listHeaders req
   , isSecure = rqIsSecure
   -- , remoteHost = SockAddrInet rqClientPort rqClientAddr
-  , pathInfo = T.splitOn "/" . T.decodeUtf8 $ rqPathInfo
+  , pathInfo = H.decodePathSegments rqPathInfo
   , queryString = paramsToQuery rqParams
   , requestBody = readSnapRequest rqBody
   , vault = mempty
-  , requestBodyLength = Wai.ChunkedBody
+  , requestBodyLength = maybe Wai.ChunkedBody Wai.KnownLength rqContentLength
   , requestHeaderHost = Just rqHostName
-  -- , requestHeaderRange = _
-  -- , requestHeaderReferer = _
-  -- , requestHeaderUserAgent = _
+  , requestHeaderRange = getHeader "Range" req
+  , requestHeaderReferer = getHeader "Referer" req
+  , requestHeaderUserAgent = getHeader "User-Agent" req
   }
 
 readSnapRequest :: InputStream ByteString -> IO ByteString
-readSnapRequest = fmap (fromMaybe B8.empty) . Stream.read
+-- readSnapRequest = fmap (fromMaybe B8.empty) . Stream.read
+readSnapRequest s = do
+  putStrLn "readSnapRequest"
+  chunk <- Stream.read s
+  putStrLn $ "chunk is " ++ show chunk
+  pure (fromMaybe B8.empty chunk)
 
 convertRequestHeaders :: [(CI ByteString, ByteString)] -> H.RequestHeaders
 convertRequestHeaders = id
@@ -91,33 +97,31 @@ responseBase (H.Status code reason) hdrs =
   setResponseStatus code reason .
   addHeaders (convertResponseHeaders hdrs)
 
+addHeaders :: Snap.HasHeaders a => [(CI ByteString, ByteString)] -> a -> a
+addHeaders hs a = foldl' (\xs (k,v) -> Snap.addHeader k v xs) a hs
+
+convertResponseHeaders :: H.ResponseHeaders -> [(CI ByteString, ByteString)]
+convertResponseHeaders = id
+
+-- | Convert a 'ResponseBuilder'
 responseBuilder :: Builder -> Snap.Response -> Snap.Response
 responseBuilder builder = setResponseBody $ \out -> do
+  putStrLn "ResponseBuilder writing"
   Stream.write (Just builder) out
   pure out
 
+-- | Convert a 'ResponseStream'
 responseStream :: H.Status -> Bool -> Wai.StreamingBody -> Snap.Response -> Snap.Response
 responseStream st reqIsHead strmbdy
   | noBody st = id
   | reqIsHead = id
   | otherwise = responseStreaming strmbdy
 
-responseFile :: FilePath -> Maybe Wai.FilePart -> Snap.Response -> Snap.Response
-responseFile path mpart r = r { rspBody = Snap.SendFile path (convertRange <$> mpart) }
-
-convertRange :: Wai.FilePart -> (Word64, Word64)
-convertRange (Wai.FilePart offset count _) = (fromIntegral offset, fromIntegral count)
-
 responseStreaming :: Wai.StreamingBody -> Snap.Response -> Snap.Response
 responseStreaming getBody = setResponseBody $ \out -> do
+  putStrLn "ResponseStreaming"
   getBody (\b -> Stream.write (Just b) out) (pure ())
   pure out
-
-convertResponseHeaders :: H.ResponseHeaders -> [(CI ByteString, ByteString)]
-convertResponseHeaders = id
-
-addHeaders :: Snap.HasHeaders a => [(CI ByteString, ByteString)] -> a -> a
-addHeaders hs a = foldl' (\xs (k,v) -> Snap.addHeader k v xs) a hs
 
 noBody :: H.Status -> Bool
 noBody = not . hasBody
@@ -131,3 +135,10 @@ hasBody s = sc /= 204
 
 isHead :: Wai.Request -> Bool
 isHead = (== H.methodHead) . requestMethod
+
+-- | Convert a 'ResponseFile'
+responseFile :: FilePath -> Maybe Wai.FilePart -> Snap.Response -> Snap.Response
+responseFile path mpart r = r { rspBody = Snap.SendFile path (convertRange <$> mpart) }
+
+convertRange :: Wai.FilePart -> (Word64, Word64)
+convertRange (Wai.FilePart offset count _) = (fromIntegral offset, fromIntegral count)
--- test/Main.hs
+++ test/Main.hs
@@ -5,14 +5,22 @@
 
 module Main where
 
-import qualified Data.ByteString.Char8 as S8
-import qualified Data.Map              as M
-import           Data.Proxy            (Proxy (..))
-import           Data.Text             (Text)
+import           Control.Concurrent.Async (race_)
+import           Control.Concurrent.MVar  (newEmptyMVar, putMVar, takeMVar)
+import           Control.Monad            (void)
+import           Data.Aeson               (Value)
+import qualified Data.ByteString.Char8    as S8
+import qualified Data.ByteString.Lazy     as BL
+import qualified Data.Map                 as M
+import           Data.Proxy               (Proxy (..))
+import           Data.Text                (Text)
+import qualified Network.HTTP.Simple      as Http
 import           Network.HTTP.Types
 import           Network.Wai
-import           Servant               ((:>), Get, JSON, Server)
-import           Servant.Server        (serve)
+import           Servant.API              ((:<|>) (..), (:>), Get, JSON, Post,
+                                           ReqBody)
+import           Servant.Server           (Server, serve)
+import           Snap.Http.Server         (httpServe, setPort, setStartupHook)
 import           Snap.Test
 import           Snap.Wai
 import           Test.Hspec
@@ -20,38 +28,47 @@ import           Test.Hspec
 main :: IO ()
 main = hspec $ do
   describe "plain wai" $ do
+    let sh = serveWai waiApp
     it "handles requests" $ do
-      let sh = serveWai waiApp
       res <- runHandler (get "foo/bar" M.empty) sh
-
-      body <- getResponseBody res
-      S8.putStrLn "body is:"
-      S8.putStrLn body
-
       assertSuccess res
       assertBodyContains "Hello" res
 
   describe "servant api" $ do
-    it "handles requests" $ do
-      let sh = serveWai servantApp
-      res <- runHandler (get "api" M.empty) sh
-
-      body <- getResponseBody res
-      S8.putStrLn "body is:"
-      S8.putStrLn body
-
+    let sh = serveWai servantApp
+    it "handles get requests" $ do
+      res <- runHandler (get "api/hello" M.empty) sh
       assertSuccess res
       assertBodyContains "Hello" res
 
+    it "handles post requests" $ do
+      res <- runHandler (postRaw "api/echo" "application/json" "{ \"yolo\": true }") sh
+      assertSuccess res
+      assertBodyContains "yolo" res
+
+  describe "integration test" $ do
+    let sh = serveWai servantApp
+    it "works with a server" $ do
+      started <- newEmptyMVar
+      let cfg = setPort 12345 $ setStartupHook (putMVar started) mempty
+      race_ (httpServe cfg sh) $ do
+        void $ takeMVar started
+        res <- Http.httpBS "http://localhost:1234/api/hello"
+        Http.getResponseStatusCode res `shouldBe` 200
+        Http.getResponseBody res `shouldSatisfy` S8.isInfixOf "Hello"
+
 waiApp :: Application
-waiApp _req respond = do
-  putStrLn "responding"
+waiApp req respond = do
+  body <- BL.toStrict <$> strictRequestBody req
+  S8.putStrLn $ "the request body is " <> body
   respond $ responseLBS status200 [] "Hello Wai"
 
-type Api = "api" :> Get '[JSON] Text
+type Api = "api" :>
+  ( "hello" :> Get '[JSON] Text
+  :<|> "echo" :> ReqBody '[JSON] Value :> Post '[JSON] Value )
 
 servantApp :: Application
 servantApp = serve (Proxy :: Proxy Api) server
 
 server :: Server Api
-server = pure "Hello Servant"
+server = pure "Hello Servant" :<|> pure
